6171b433b919c4aa6ec16de0aaed88c1f769924a
Nahuel Tori <nahuel.tori@avature.net>
2017-04-24 15:16:51 -0300

    Adding new CORS version 2

--- src/main/java/sikim/MyConfiguration.java
+++ src/main/java/sikim/MyConfiguration.java
@@ -1,9 +1,10 @@
 package sikim;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.*;
 import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.cors.*;
+
 
 @Configuration
 public class MyConfiguration {
